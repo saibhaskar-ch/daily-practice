eacb29a0e651be25c78457896e09c6b119a74ca3
Sai Bhaskar Cherla <saibhaskarcherla@Sais-MacBook-Pro.local>
2026-04-23 17:55:32 -0500

    [Mood Logger] Frontend Add title

--- mood-logger/mood-logger-frontend/src/App.tsx
+++ mood-logger/mood-logger-frontend/src/App.tsx
@@ -1,5 +1,14 @@
 const App = () => {
-  return <div className="min-h-screen bg-dot-grid bg-white">App</div>;
+  return <div className="min-h-screen bg-dot-grid bg-stone-50 flex justify-center">
+    <div className="w-full md:max-w-270">
+
+          <header className="md:mt-10">
+            <h2 className="text-lg font-semibold">mood_logger</h2>
+          </header>
+    </div>
+    
+    
+    </div>;
 };
 
 export default App;
